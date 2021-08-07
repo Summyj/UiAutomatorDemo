094f34f14efff9ad6ff98639f2378c0786fc778b
Summyj <18829551943@163.com>
2021-08-07 14:49:46 +0800

    try use env

--- app/src/androidTest/java/com/example/androidtest/tests/LoginApp.kt
+++ app/src/androidTest/java/com/example/androidtest/tests/LoginApp.kt
@@ -11,8 +11,8 @@ class LoginApp{
     private lateinit var device: UiDevice
     private val basePage: BasePage = BasePage()
     private val loginPage: LoginPage = LoginPage()
-    private val email: String = "test@test.com"
-    private val password: String = "Test123456"
+    private val email: String = System.getenv("TEST_ACCOUNT")
+    private val password: String = System.getenv("TEST_PASSWORD")
 
     @Before
     fun openApp() {
